f49dd0926022b19bd35ac729b1fb33a0c4393f24
toan-pn <toanphamhsgs@gmail.com>
2019-03-28 23:01:47 +0700

    Add project files.

---
+++ BlogPageMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BlogPageMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
